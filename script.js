496c0a99588a144e12b16c696543f89a35b0d861
Lynne West <thislynnewest@gmail.com>
2018-10-15 20:37:03 -0700

    projects test

--- script.js
+++ script.js
@@ -112,11 +112,11 @@ var picH;
 		sparks()
 	});
 
-	// $("#projects-id").onload(function()
-	// {
-	// 	$(".navbar").addClass("scroll-nav");
-	// 	console.log("projects-id loaded");
-	// });
+	$("href=projects-id").onload(function()
+	{
+		$(".navbar").addClass("scroll-nav");
+		console.log("projects-id loaded");
+	});
 
 	$(document).scroll(function() 
 	{
